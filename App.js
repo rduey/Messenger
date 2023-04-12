24a346d1c6b0281afed1054b9c32509af6d37d8e
rduey <rduey@ucsc.edu>
2023-04-11 19:47:36 -0700

    added dependencies

---
+++ App.js
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2022 David C. Harrison. All right reserved.
+ *
+ * You may not use, distribute, publish, or modify this code without
+ * the express written permission of the copyright holder.
+ */
+
+/* **************************************************************************
+ * DO NOT MODIFY THIS FILE
+ * **************************************************************************/
+
+import React from 'react';
+import Main from './src/Main';
+
+const App = () => {
+  return <Main />;
+};
+
+export default App;
